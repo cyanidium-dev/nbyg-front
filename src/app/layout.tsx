80ea5c93420026518c020802a9d79c5718b50548
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-09 20:33:56 +0200

    deleted unnecessary files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="da">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
