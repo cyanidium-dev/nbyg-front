b91cc4347be6502fdd8e28d7847799f464129142
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-21 18:26:22 +0200

    added faq animations

--- src/components/shared/sections/faqSection/FaqItem.tsx
+++ src/components/shared/sections/faqSection/FaqItem.tsx
@@ -25,17 +25,21 @@ export default function FaqItem({ faqItem, idx }: FaqItemProps) {
       viewport={{ once: true, amount: 0.2 }}
       variants={listItemVariantsLeft}
       onClick={toggleShowMore}
-      className={`relative group cursor-pointer px-4 py-6 lg:py-[26px] rounded-[8px] backdrop-blur-[38px] button-shadow-white transition-[background-color,filter] duration-700 ease-in-out`}
+      className={`relative group cursor-pointer px-4 py-6 lg:py-[26px] rounded-[8px] backdrop-blur-[38px]`}
     >
       <div
         className="absolute z-10 inset-0 rounded-[8px] bg-white/6 shadow-[inset_0px_4px_12.6px_rgba(255,255,255,0.12)] pointer-events-none 
-      xl:group-hover:brightness-125 transition duration-300 ease-in-out"
+      "
       />
       <div className={`flex items-center gap-6 justify-between`}>
-        <h3 className={`text-[16px] lg:text-[18px] font-normal leading-[120%]`}>
+        <h3
+          className={`w-full text-[16px] lg:text-[18px] font-normal leading-[120%] shadow-white-universal`}
+        >
           {question}
         </h3>
-        <ShevronIcon className={`size-6 shrink-0 rotate-180`} />
+        <ShevronIcon
+          className={`size-6 shrink-0 rotate-180 svg-shadow-white`}
+        />
       </div>
       <div
         className={`overflow-hidden transition-[max-height] duration-700 will-change-transform ${
--- src/components/shared/sections/faqSection/FaqSection.tsx
+++ src/components/shared/sections/faqSection/FaqSection.tsx
@@ -2,6 +2,8 @@ import type { FaqSection as FaqSectionData } from "@/types/page";
 import Container from "../../container/Container";
 import SectionTitle from "../../titles/SectionTitle";
 import FaqList from "./FaqList";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
 interface FaqSectionProps extends FaqSectionData {
   uniqueKey?: string;
@@ -14,13 +16,23 @@ const FaqSection = (_props: FaqSectionProps) => {
       <Container className="flex flex-col gap-10 lg:gap-12 pt-25 lg:pt-[138px]">
         <div>
           <SectionTitle
-            key={`${uniqueKey}-faq-section`}
+            key={`${uniqueKey}-faq-section-title`}
             className="text-[24px] lg:text-[64px] font-light leading-[120%] lg:text-right"
           >
             FAQ
           </SectionTitle>
           {description ? (
-            <p className="mt-8 lg:mt-4 lg:text-right">{description}</p>
+            <motion.p
+              key={`${uniqueKey}-faq-section-description`}
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.1 }}
+              variants={fadeInAnimation({ scale: 0.85, x: 30, delay: 0.3 })}
+              className="mt-8 lg:mt-4 lg:text-right"
+            >
+              {description}
+            </motion.p>
           ) : null}
         </div>
         <FaqList faqList={items} />
