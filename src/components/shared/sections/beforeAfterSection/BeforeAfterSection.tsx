aea051814d2017befc580b94df5ad25f3dadd4f2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-27 14:03:43 +0200

    added title with decorations

--- src/components/shared/sections/beforeAfterSection/BeforeAfterSection.tsx
+++ src/components/shared/sections/beforeAfterSection/BeforeAfterSection.tsx
@@ -1,8 +1,30 @@
-import type { BeforeAfterSection as BeforeAfterSectionData } from "@/types/page";
+import Container from "../../container/Container";
+import { BeforeAfterSection as BeforeAfterSctionData } from "@/types/page";
+import SectionTitle from "../../titles/SectionTitle";
+import DecorativeEllipsis from "../../decorativeEllipsis/DecorativeEllipsis";
 
-const BeforeAfterSection = (_props: BeforeAfterSectionData) => {
-  return null;
+interface BeforeAfterSctionProps extends BeforeAfterSctionData {
+  uniqueKey?: string;
+}
+
+const BeforeAfterSection = (_props: BeforeAfterSctionProps) => {
+  const { items, uniqueKey } = _props;
+
+  if (!items || !items?.length) return null;
+
+  return (
+    <section>
+      <Container className="py-25 lg:pt-[138px] lg:pb-0">
+        <div className="relative lg:max-w-[641px]">
+          <DecorativeEllipsis
+            uniqueKey={uniqueKey}
+            className="absolute -top-[26px] lg:top-auto left-0 lg:left-auto lg:right-0 lg:bottom-5"
+          />
+          <SectionTitle>Se vores projekter før og efter</SectionTitle>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default BeforeAfterSection;
-
