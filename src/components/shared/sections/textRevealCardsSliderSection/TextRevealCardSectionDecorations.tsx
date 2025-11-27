26b0e093ff8a125636b32675cbeee1bd63afd89a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-27 19:56:36 +0200

    added decorations for desktop

--- src/components/shared/sections/textRevealCardsSliderSection/TextRevealCardSectionDecorations.tsx
+++ src/components/shared/sections/textRevealCardsSliderSection/TextRevealCardSectionDecorations.tsx
@@ -10,7 +10,7 @@ export default function TextRevealCardsSectionDecorations({
   uniqueKey,
 }: TextRevealCardsSectionDecorationsProps) {
   return (
-    <div className={`absolute w-full h-full pointer-events-none`}>
+    <div className={`absolute -z-10 w-full h-full pointer-events-none`}>
       <motion.div
         key={`${uniqueKey}-decorations`}
         initial="hidden"
@@ -18,7 +18,7 @@ export default function TextRevealCardsSectionDecorations({
         exit="exit"
         viewport={{ once: true, amount: 0.1 }}
         variants={fadeInAnimation({ scale: 0.85, delay: 0, duration: 0.5 })}
-        className={`absolute top-[-101px] right-[calc(50%-282px)] xs:right-[calc(50%-400px)] sm:right-[calc(50%-500px)] md:right-[calc(50%-600px)] lg:top-[-328px] lg:right-[calc(50%-500px)]"} 
+        className={`absolute top-[-101px] lg:top-auto lg:bottom-[-101px] right-[calc(50%-282px)] xs:right-[calc(50%-400px)] sm:right-[calc(50%-500px)] md:right-[calc(50%-600px)] lg:right-[calc(50%-330px)] 
         w-[337px] lg:w-[634px] h-auto aspect-337/421`}
       >
         <Image
@@ -28,10 +28,10 @@ export default function TextRevealCardsSectionDecorations({
           alt="ellipsis"
           className={`w-[360px] lg:w-[739px] h-auto -rotate-10`}
         />
-        {/* <div
-          className={`hidden lg:block absolute  w-[1134px] h-[504px] rounded-full bg-black supports-backdrop-filter:blur-[53px] 
-            will-change-transform top-14 right-[-109px]`}
-        /> */}
+        <div
+          className={`hidden lg:block absolute w-[1477px] h-[762px] rounded-full bg-black supports-backdrop-filter:blur-[53px] 
+            will-change-transform top-[-122px] right-[-177px]`}
+        />
       </motion.div>
     </div>
   );
