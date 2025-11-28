b49616a0a8700714348dbbe4ea172dcff6b50263
Shade <kulakaa073@gmail.com>
2025-11-28 12:46:39 +0200

    fix: adding gradients

--- src/components/homePage/hero/HeroGallery.tsx
+++ src/components/homePage/hero/HeroGallery.tsx
@@ -28,6 +28,8 @@ const HeroGallerySlider = dynamic(() => import("./HeroGallerySlider"), {
 export default function HeroGallery() {
     return (
         <div className="absolute inset-0 -z-10 rounded-b-[18px] overflow-hidden">
+            <div className="md:hidden absolute inset-0 z-10 pointer-events-none bg-[linear-gradient(240.18deg,rgba(0,0,0,0)_19.24%,rgba(0,0,0,0.8)_82.96%)]" />
+            <div className="absolute inset-0 z-10 pointer-events-none bg-[linear-gradient(0deg,rgba(0,0,0,0)_-36.89%,rgba(0,0,0,0.464)_91.14%,rgba(0,0,0,0)_144.39%)]" />
             <HeroGallerySlider images={HERO_GALLERY_IMAGES} />
         </div>
     );
