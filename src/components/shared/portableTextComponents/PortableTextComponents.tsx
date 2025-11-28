bfdb7073f7743a5c8e8dac8da15609ae973f07df
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-11-28 17:44:38 +0200

    Update PortableTextComponents.tsx

--- src/components/shared/portableTextComponents/PortableTextComponents.tsx
+++ src/components/shared/portableTextComponents/PortableTextComponents.tsx
@@ -2,7 +2,7 @@ import type { PortableTextComponents } from "@portabletext/react";
 
 export const portableTextComponents: Partial<PortableTextComponents> = {
   block: {
-    normal: ({ children }) => <p className="not-last:mb-2">{children}</p>,
+    normal: ({ children }) => <p className="not-last:mb-3">{children}</p>,
   },
   list: {
     bullet: ({ children }) => (
@@ -24,6 +24,8 @@ export const portableTextComponents: Partial<PortableTextComponents> = {
   },
   listItem: {
     bullet: ({ children }) => <li>{children}</li>,
-    number: ({ children }) => <li>{children}</li>,
+    number: ({ children }) => (
+      <li className="not-last:mb-5 lg:not-last:mb-9">{children}</li>
+    ),
   },
 };
