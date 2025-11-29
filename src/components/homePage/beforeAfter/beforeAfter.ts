26c0c8a41605124f8944f7d1143253a37af1a5da
Shade <kulakaa073@gmail.com>
2025-11-29 11:16:17 +0200

    feat: home page before-after section

---
+++ src/components/homePage/beforeAfter/beforeAfter.ts
@@ -0,0 +1,14 @@
+export const BEFORE_AFTER_IMAGES = [
+    {
+        beforeImage: "/images/homePage/beforeAfter/slide-1-before.webp",
+        afterImage: "/images/homePage/beforeAfter/slide-1-after.webp",
+    },
+    {
+        beforeImage: "/images/homePage/beforeAfter/slide-2-before.webp",
+        afterImage: "/images/homePage/beforeAfter/slide-2-after.webp",
+    },
+    {
+        beforeImage: "/images/homePage/beforeAfter/slide-3-before.webp",
+        afterImage: "/images/homePage/beforeAfter/slide-3-after.webp",
+    },
+];
