59066041c8a4f12971eae1c1b7709464dec8d28b
Shade <kulakaa073@gmail.com>
2025-11-30 08:39:51 +0200

    feat: bottom cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/homePage/hero/Hero";
+import BottomCTA from "@/components/homePage/bottomCTA/BottomCTA";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <BottomCTA />
         </>
     );
 }
