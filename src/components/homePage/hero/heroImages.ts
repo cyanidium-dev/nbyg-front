cf43e53844d95394bd6f2006ed962dff87e2f67e
Shade <kulakaa073@gmail.com>
2025-12-01 17:20:44 +0200

    chore: cleanup

--- src/components/homePage/hero/heroImages.ts
+++ src/components/homePage/hero/heroImages.ts
@@ -5,27 +5,27 @@ export interface HeroGalleryImage {
 
 export const HERO_GALLERY_IMAGES: HeroGalleryImage[] = [
     {
-        url: "/images/homePage/hero/gallery-11.webp",
+        url: "/images/homePage/hero/gallery-1.webp",
         blurDataURL:
             "data:image/webp;base64,UklGRjgAAABXRUJQVlA4ICwAAACwAQCdASoKAAUABUB8JagCdADcyyoAAP6zYS/tuKOvL4ZyBTj6eNG0E+eoAA==",
     },
     {
-        url: "/images/homePage/hero/gallery-21.webp",
+        url: "/images/homePage/hero/gallery-2.webp",
         blurDataURL:
             "data:image/webp;base64,UklGRjwAAABXRUJQVlA4IDAAAACwAQCdASoKAAUABUB8JagCdAEO1upQAP14uLNLAro7yeRYa/IhcIxRQFlEgcaIAAA=",
     },
     {
-        url: "/images/homePage/hero/gallery-31.webp",
+        url: "/images/homePage/hero/gallery-3.webp",
         blurDataURL:
             "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACQAQCdASoKAAUABUB8JQBOgBraRiAA/oEAeDk6gcKxDsQShZKgAA==",
     },
     {
-        url: "/images/homePage/hero/gallery-41.webp",
+        url: "/images/homePage/hero/gallery-4.webp",
         blurDataURL:
             "data:image/webp;base64,UklGRjQAAABXRUJQVlA4ICgAAACwAQCdASoKAAUABUB8JQBOgCHM/jvAAP545AdiKdL82DvjiMM+BgAA",
     },
     {
-        url: "/images/homePage/hero/gallery-51.webp",
+        url: "/images/homePage/hero/gallery-5.webp",
         blurDataURL:
             "data:image/webp;base64,UklGRkAAAABXRUJQVlA4IDQAAADQAQCdASoKAAUABUB8JagCdAEXYhUJEAD77DbwcITZl5QwYjlXF36q/xG62xEBIKKSgAAA",
     },
