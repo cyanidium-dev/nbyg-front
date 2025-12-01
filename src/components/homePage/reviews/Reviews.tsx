5398005192f8190ffca4dd76e4efc8337bcd1c6b
Shade <kulakaa073@gmail.com>
2025-12-01 17:50:10 +0200

    update

--- src/components/homePage/reviews/Reviews.tsx
+++ src/components/homePage/reviews/Reviews.tsx
@@ -10,17 +10,31 @@ export default function Reviews() {
         <section className="py-25 relative">
             <ReviewsDecorations />
             <Container>
-                <SectionTitle className="mb-8">Anmeldelser</SectionTitle>
-                <p className="mb-10 text-[18px] leading-[111%] font-light">
-                    Hvad vores kunder siger om os
-                </p>
+                <div className="mb-10 lg:mb-[78px] md:flex items-center justify-between">
+                    <div>
+                        <SectionTitle className="mb-8 lg:text-[64px] md:mb-[59px]">
+                            Anmeldelser
+                        </SectionTitle>
+                        <p className="text-[18px] leading-[111%] font-light">
+                            Hvad vores kunder siger om os
+                        </p>
+                    </div>
+                    <div className="relative hidden md:block h-[156px] w-[46.12%] rounded-[8px] overflow-hidden">
+                        <Image
+                            src="/images/homePage/reviews/reviewImage.webp"
+                            alt="Reviews"
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
+                </div>
                 <ReviewsBlock reviews={reviewsData} />
-                <div className="relative mt-[54px] w-full h-[156px] rounded-[8px] overflow-hidden">
+                <div className="md:hidden relative mt-[54px] w-full h-[156px] rounded-[8px] overflow-hidden">
                     <Image
                         src="/images/homePage/reviews/reviewImage.webp"
                         alt="Reviews"
                         fill
-                        className="object-cover"
+                        className="object-cover object-top"
                     />
                 </div>
             </Container>
--- src/components/homePage/reviews/ReviewsDecorations.tsx
+++ src/components/homePage/reviews/ReviewsDecorations.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ReviewsDecorations() {
     return (
         <div className="absolute -z-10 inset-0 pointer-events-none">
-            <div className="absolute right-[-154px] top-[13px] rotate-[-34deg]">
+            <div className="lg:hiddenabsolute right-[-154px] top-[13px] rotate-[-34deg]">
                 <Image
                     src="/images/decorations/ellipsis.svg"
                     alt="ellipsis"
