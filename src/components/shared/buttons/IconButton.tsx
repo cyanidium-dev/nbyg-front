8fe2edf61ec02cee71894988a7d766f2ec78fbc8
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-02 18:07:29 +0200

    updated icon button

--- src/components/shared/buttons/IconButton.tsx
+++ src/components/shared/buttons/IconButton.tsx
@@ -16,7 +16,7 @@ export default function IconButton({
       type="button"
       aria-label="icon button"
       onClick={handleClick}
-      className={`cursor-pointer outline-none before:content-[''] before:absolute before:-z-10 before:top-0 before:left-0 before:rounded-full before:size-full 
+      className={`cursor-pointer flex items-center justify-center outline-none before:content-[''] before:absolute before:-z-10 before:top-0 before:left-0 before:rounded-full before:size-full 
                         before:bg-white before:blur-[4px] supports-backdrop-filter:before:blur-xs will-change-transform before:opacity-0
                          before:transition before:duration-300 before:ease-out active:before:opacity-20 
                         focus-visible:before:opacity-20 lg:hover:before:opacity-20 ${className}`}
