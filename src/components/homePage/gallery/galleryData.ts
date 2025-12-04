fd262e4864050185e074dee097988759828a9c8c
Shade <kulakaa073@gmail.com>
2025-12-04 13:41:10 +0200

    feat: gallery section

--- src/components/homePage/gallery/galleryData.ts
+++ src/components/homePage/gallery/galleryData.ts
@@ -4,7 +4,6 @@ export const galleryData = [
         image: "/images/homePage/gallery/gallery-1.webp",
     },
     {
-  
         _key: "2",
         image: "/images/homePage/gallery/gallery-2.webp",
     },
@@ -19,5 +18,73 @@ export const galleryData = [
     {
         _key: "5",
         image: "/images/homePage/gallery/gallery-5.webp",
+    },
+    {
+        _key: "6",
+        image: "/images/homePage/gallery/gallery-6.webp",
+    },
+    {
+        _key: "7",
+        image: "/images/homePage/gallery/gallery-7.webp",
+    },
+    {
+        _key: "8",
+        image: "/images/homePage/gallery/gallery-8.webp",
+    },
+    {
+        _key: "9",
+        image: "/images/homePage/gallery/gallery-9.webp",
+    },
+    {
+        _key: "10",
+        image: "/images/homePage/gallery/gallery-10.webp",
+    },
+    {
+        _key: "11",
+        image: "/images/homePage/gallery/gallery-11.webp",
+    },
+    {
+        _key: "12",
+        image: "/images/homePage/gallery/gallery-12.webp",
+    },
+    {
+        _key: "13",
+        image: "/images/homePage/gallery/gallery-13.webp",
+    },
+    {
+        _key: "14",
+        image: "/images/homePage/gallery/gallery-14.webp",
+    },
+    {
+        _key: "15",
+        image: "/images/homePage/gallery/gallery-15.webp",
+    },
+    {
+        _key: "16",
+        image: "/images/homePage/gallery/gallery-16.webp",
+    },
+    {
+        _key: "17",
+        image: "/images/homePage/gallery/gallery-17.webp",
+    },
+    {
+        _key: "18",
+        image: "/images/homePage/gallery/gallery-18.webp",
+    },
+    {
+        _key: "19",
+        image: "/images/homePage/gallery/gallery-19.webp",
+    },
+    {
+        _key: "20",
+        image: "/images/homePage/gallery/gallery-20.webp",
+    },
+    {
+        _key: "21",
+        image: "/images/homePage/gallery/gallery-21.webp",
+    },
+    {
+        _key: "22",
+        image: "/images/homePage/gallery/gallery-22.webp",
     },
 ];
