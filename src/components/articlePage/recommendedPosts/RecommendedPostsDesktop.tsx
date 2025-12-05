1093f4481956e95ae2a3e70d25f96b86c113254a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-05 11:11:40 +0200

    added recommended products for mobile

---
+++ src/components/articlePage/recommendedPosts/RecommendedPostsDesktop.tsx
@@ -0,0 +1,11 @@
+import { BlogPostPreview } from "@/types/blogPost";
+
+interface RecommendedPostsDesktopProps {
+  posts: BlogPostPreview[];
+}
+
+export default function RecommendedPostsDesktop({
+  posts,
+}: RecommendedPostsDesktopProps) {
+  return <div>RecommendedPostsDesktop</div>;
+}
