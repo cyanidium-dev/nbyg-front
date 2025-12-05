b6c871a8a2695ce4c7ed3081d4a16b336fdbd921
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-05 18:21:17 +0200

    added decorations and animations

--- src/components/byggeydelserPage/services/ServiceSection.tsx
+++ src/components/byggeydelserPage/services/ServiceSection.tsx
@@ -4,6 +4,9 @@ import DecorativeEllipsis from "@/components/shared/decorativeEllipsis/Decorativ
 import SectionTitle from "@/components/shared/titles/SectionTitle";
 import Image from "next/image";
 import Link from "next/link";
+import ServiceSectionDecorations from "./ServiceSectionDecorations";
+import * as motion from "motion/react-client";
+import { fadeInAnimation, headerVariants } from "@/utils/animationVariants";
 
 interface ServiceSectionProps {
   service: {
@@ -20,14 +23,28 @@ export default function ServiceSection({ service }: ServiceSectionProps) {
   const { text, url } = button;
 
   return (
-    <section className="py-25 lg:pt-[152px] lg:pb-0">
+    <section className="relative py-25 lg:pt-[152px] lg:pb-0">
+      <ServiceSectionDecorations uniqueKey={title} imageRight={imageRight} />
       <Container
         className={`relative ${imageRight ? "md:flex" : "md: flex flex-row-reverse"} gap-9`}
       >
         <div>
           <DecorativeEllipsis className="absolute md:static -top-[44px] left-4 md:mb-9" />
           <SectionTitle className="mb-8 lg:mb-9">{title}</SectionTitle>
-          <div className="md:hidden relative h-[328px] mb-8 rounded-[12px] overflow-hidden">
+          <motion.div
+            key={`${title}-image-mobile`}
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={fadeInAnimation({
+              scale: 0.85,
+              x: 70,
+              y: 30,
+              delay: 0.4,
+            })}
+            className="md:hidden relative h-[328px] mb-8 rounded-[12px] overflow-hidden"
+          >
             <Image
               src={image}
               alt={title}
@@ -35,22 +52,61 @@ export default function ServiceSection({ service }: ServiceSectionProps) {
               sizes="(max-width: 786px) 100vw, 50vw"
               className="object-cover"
             />
-          </div>
-          <p className="mb-10 lg:mb-9">{description}</p>
-          <Link href="/url">
-            <MainButton className="h-[58px] px-9 font-medium uppercase">
-              {text}
-            </MainButton>
-          </Link>
+          </motion.div>
+          <motion.p
+            key={`${title}-description`}
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={fadeInAnimation({
+              scale: 0.85,
+              x: 70,
+              y: 30,
+              delay: 0.4,
+            })}
+            className="mb-10 lg:mb-9"
+          >
+            {description}
+          </motion.p>
+          <motion.div
+            key={`${title}-button`}
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={fadeInAnimation({
+              scale: 0.85,
+              delay: 0.4,
+            })}
+          >
+            {" "}
+            <Link href={url}>
+              <MainButton className="h-[58px] px-9 font-medium uppercase">
+                {text}
+              </MainButton>
+            </Link>
+          </motion.div>
         </div>
-        <div className="relative hidden md:block h-auto w-[43.6%] shrink-0 rounded-[12px] overflow-hidden">
+        <motion.div
+          key={`${title}-image-desktop`}
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({
+            scale: 0.85,
+            delay: 0.4,
+          })}
+          className="relative hidden md:block h-auto md:w-[30%] xl:w-[43.6%] shrink-0 rounded-[12px] overflow-hidden"
+        >
           <Image
             src={image}
             alt={title}
             fill
             sizes="(max-width: 786px) 100vw, 50vw"
           />
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
