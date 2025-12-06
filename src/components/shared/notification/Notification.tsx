bf99e67e29d05482acfb1eac9069cfeaf8d6fc0e
Shade <kulakaa073@gmail.com>
2025-12-06 07:06:34 +0200

    Revert "fix: adjusting min width"
    This reverts commit a345c537fd0274f433443979045ca87a40e3091e.

--- src/components/shared/notification/Notification.tsx
+++ src/components/shared/notification/Notification.tsx
@@ -22,9 +22,9 @@ export default function Notification({
             isModalShown={isNotificationShown}
             setIsModalShown={setIsNotificationShown}
             variant="notification"
-            className="rounded-[12px]"
+            className="rounded-[12px] min-w-[328px]"
         >
-            <div className="p-4 pt-14 md:p-6 md:pt-14 lg:p-10 relative flex flex-col justify-center items-center w-full min-w-[256px] max-w-[560px] z-20">
+            <div className="p-4 pt-14 md:p-6 md:pt-14 lg:p-10 relative flex flex-col justify-center items-center w-full max-w-[560px] z-20">
                 <h3 className="mb-8 text-[24px] lg:text-[48px] font-find-sans-pro font-light leading-[120%] text-center uppercase">
                     {title}
                 </h3>
