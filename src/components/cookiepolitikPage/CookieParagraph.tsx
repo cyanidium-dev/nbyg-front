d7ca1d43cfe3cde95d556beeec366b6de0d58887
Shade <kulakaa073@gmail.com>
2025-12-08 11:49:29 +0200

    fix: splitting cookies page into components

---
+++ src/components/cookiepolitikPage/CookieParagraph.tsx
@@ -0,0 +1,30 @@
+import { twMerge } from "tailwind-merge";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
+
+interface CookieParagraphProps {
+    children: React.ReactNode;
+    className?: string;
+    delay?: number;
+}
+export default function CookieParagraph({
+    children,
+    className,
+    delay = 0,
+}: CookieParagraphProps) {
+    return (
+        <motion.p
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.1 }}
+            variants={fadeInAnimation({ scale: 0.95, y: 30, delay: delay })}
+            className={twMerge(
+                "font-light leading-[150%] tracking-[-0.02rem] text-[14px] lg:text-[16px]",
+                className
+            )}
+        >
+            {children}
+        </motion.p>
+    );
+}
